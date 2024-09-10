34dfeeac0837a5622e0641ea73c082c5ef93c345
unknown <abdullahmmmm64@gmail.com>
2024-09-10 12:24:21 +0300

    Initial commit with project files

---
+++ Abdullah/Hospital/HospitalSystem/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+path("",views.register, name="r"),
+    path("home/",views.search_doctors , name='index'),
+    path("login/",views.login_view, name='login'),
+    path('doctor/<int:id>/', views.doctor_detail, name='doctord'),
+]
